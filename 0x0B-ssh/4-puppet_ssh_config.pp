a75272a1291ca952becd7eb48426b8062e453223
AMIRANDA9112 <andreselfm@gmail.com>
2020-08-04 23:27:59 -0500

    full ssh practice

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# client SSH configuration file
+# connect without typing a password.
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
