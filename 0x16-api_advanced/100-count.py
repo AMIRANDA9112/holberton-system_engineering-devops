88f4f3d2f482d313219e895e1f52cb85e21422e7
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-21 23:51:20 -0500

    100 - main error :(

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-"""
-100-main
-"""
-import sys
+""" Request recursively the top ten hot posts """
 
-if __name__ == '__main__':
+import requests
 
-    words = __import__('100-count').words
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
 
+def count_words(subreddit, word_list, after=None, my_dict={}):
+    """ API queri subreddit """
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    query = requests.get(url, allow_redirects=False,
+                         params={'after': after},
+                         headers={'User-Agent': 'Pear'})
+    if query and query.status_code == 200:
+        list_query = query.json().get('data').get('children')
+        for children in list_query:
+            title1 = children.get('data').get('title')
+            for word in word_list:
+                try:
+                    my_dict[word] += title1.lower().split().count(word.lower())
+                except KeyError:
+                    my_dict[word] = title1.lower().split().count(word.lower())
+        after = query.json().get('data').get('after')
+
+        if after is None:
+            for key, val in sorted(my_dict.items(), key=lambda x: x[1],
+                                   reverse=True):
+                if val != 0:
+                    print("{}: {}".format(key, val))
+            return
+        return count_words(subreddit, word_list, after, my_dict)
     else:
-        funct = [x for x in sys.argv[2].split()]
-        result = words(sys.argv[1], funct)
+        return
