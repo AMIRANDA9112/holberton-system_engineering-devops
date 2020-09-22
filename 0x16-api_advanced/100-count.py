e6a1ff6d401e34bb7d2163f9dd0b0f6f6be20a7a
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-21 23:43:00 -0500

    100 - main

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API,
-parses the title of all hot articles,
-and prints a sorted count of given keywords
+100-main
 """
 import sys
 
 if __name__ == '__main__':
+
     words = __import__('100-count').words
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
 
-        result = words(sys.argv[1], [x for x in sys.argv[2].split()])
+    else:
+        funct = [x for x in sys.argv[2].split()]
+        result = words(sys.argv[1], funct)
